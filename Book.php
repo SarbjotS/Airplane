22272bad42d382721252613453e1cb94616bc544
SarbjotS <62689363+SarbjotS@users.noreply.github.com>
2020-10-16 20:52:50 +1300

    Update Book.php

--- Book.php
+++ Book.php
@@ -18,17 +18,18 @@ while($tb1 = mysqli_fetch_array($temp1))
 $query = "SELECT * FROM routes";
 $temp = $temp1 = mysqli_query($connect, $query);
 
-
 while($tb2 = mysqli_fetch_array($temp1))
 {
     $route = $route."<tr><td>$tb2[0]</td><td>$tb2[1]</td><td>$tb2[2]</td><td>$tb2[3]</td></tr>";
 }
-$query ="SELECT * FROM flights";
-$temp = $temp1 = mysqli_query($connect, $query);
+
+$query ="SELECT routeID,flightID,flightDate,flightTime FROM flights";
+$temp1 = mysqli_query($connect, $query);
 
 while($tb3 = mysqli_fetch_array($temp1))
 {
-    $flights = $flights."<tr><td>$tb3[0]</td><td>$tb3[1]</td><td>$tb3[2]</td><td>";
+    $flights = $flights."<tr><td>$tb3[0]</td><td>$tb3[1]</td><td>$tb3[2]<td>$tb3[3]</td></td>";
+    // echo $flights;
 }
 ?>
 <!DOCTYPE html>
@@ -43,6 +44,10 @@ while($tb3 = mysqli_fetch_array($temp1))
     <script src="SearchFunction.js" ></script>
     <script src="BrowseInformation.js" ></script>
     <script src="ShowFlightOnBrowse.js" ></script>
+    <script src="ReturnAvailableFlights.js"></script>
+    <script src="SearchRoutes.js"></script>
+
+
 
 
     <title>Book!</title>
@@ -52,7 +57,7 @@ while($tb3 = mysqli_fetch_array($temp1))
 <ul>
     <li><a href="index.html">Home</a></li>
     <li><a href="Book.php">Book</a></li>
-    <li><a href="Browse.php">Browse</a></li>
+    <li ><a href="Browse.php">Browse</a></li>
     <li><a href="Cancel.php">Cancel Booking</a></li>
     <li id="LoginButton"><a href="loginscreen.html">Login</a> </li>
 
@@ -69,9 +74,9 @@ while($tb3 = mysqli_fetch_array($temp1))
 
 <h2 class="A">ID</h2>
 <h4 class="A"><strong>Enter Flight ID to learn more</strong></h4>
-<input type="text" id="TakeThis" style="margin-left: 42%"
+<input type="text" id="TakeThis" style="margin-left: 42%" onkeyup="SearchRoutes()"
        placeholder="Enter ID here...">
-<button  type="button" onclick="ShowFlightOnBrowse()"> Submit </button>
+<button onclick="ShowFlightOnBrowse()" type="button" > Submit </button>
 
 
 <table style="width:75%" align="center" id="RoutesTable">
@@ -88,22 +93,33 @@ while($tb3 = mysqli_fetch_array($temp1))
 
 </table>
 <!-- Flight times taken from  https://flighttime-calculator.com/ -->
-<textarea class="Select" id="ShowBrowseFlightDetails" rows="4" cols="75";>
-    </textarea>
+<table style="width:75%" align="center" id="DateTimeTable" onclick="window.location='#';">
+    <tr>
+        <th>ID</th>
+        <th>Flight ID</th>
+        <th>Time</th>
+        <th>Date</th>
+    <?php
+    echo $flights;
+    ?>
+        <td>1</td>
+</tr>
+</table>
 
-<h4>Please Select the Flight you want to book</h4>
-<?php
-        $query = "SELECT * FROM routes";
-        $temp = $temp1 = mysqli_query($connect, $query);
-?>
-<form method="post" action="flights.php">
-    <select name="routeList">
-    <<?php foreach ($temp as $rl): ?>
-        <option value=<?= $rl['routeID'] ?>><?= $rl['point1'], ' -> ', $rl['point2']?></option>
-    <?php endforeach ?>
-    </select>
-    <input type="submit" name="submit" value="Submit">
-</form>
+<div style="margin-left:20%" >
+<h4  style="color: white; "><strong>Please enter FlightID</strong></h4>
+<button>Confirm</button>
+    <input type="text">
+
+</div>
+
+
+<!-- <textarea class="Select" style="margin-left:20% " id="ShowAvailableDates" rows="12" cols="25";>
+
+</textarea> -->
+<textarea class="Select"  style="margin-left: 20%;" id="ShowBrowseFlightDetails" rows="3" cols="60";>
+</textarea>
+
 
 </body>
 </html>
